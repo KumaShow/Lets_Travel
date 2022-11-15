1cd3ec4b2f075d44db06feffe1f55bc6af50b9bf
KumaShow <kumadth@gmail.com>
2022-11-16 00:35:02 +0800

    新增功能
    1. 表單驗證
    2. c3 圖表

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,55 +1,27 @@
 "use strict";
 
-// 改用 axios 載入外部資料
-// const tickets = [
-//   {
-//     id: 1,
-//     name: "綠島自由行套裝行程",
-//     imgUrl:
-//       "https://github.com/hexschool/2022-web-layout-training/blob/main/js_week5/travel_1.png?raw=true",
-//     area: "台北",
-//     price: 1280,
-//     num: 8,
-//     rank: 8.6,
-//     describe:
-//       "嚴選超高CP值綠島自由行套裝行程，多種綠島套裝組合，提供台東綠島來回船票、綠島環島機車、綠島民宿住宿，行程加贈『綠島浮潛體驗』以及『綠島生態導覽』，讓你用輕鬆的綠島套裝自由行，也能深度認識綠島在地文化。",
-//   },
-//   {
-//     id: 2,
-//     name: "南庄度假村露營車二日遊",
-//     imgUrl:
-//       "https://github.com/hexschool/2022-web-layout-training/blob/main/js_week5/travel_6.png?raw=true",
-//     area: "台中",
-//     price: 2480,
-//     num: 2,
-//     rank: 9.2,
-//     describe:
-//       "南庄雲水豪華露營車，快來擁有最愜意的露營體驗吧！ 一泊一食，輕鬆享受露營車樂趣。 獨立衛浴與私人戶外露臺。 入住豪華露營車還能使用戶外SPA大眾湯，感受美人湯魅力。",
-//   },
-//   {
-//     id: 3,
-//     name: "漁樂碼頭釣魚體驗套票",
-//     imgUrl:
-//       "https://github.com/hexschool/2022-web-layout-training/blob/main/js_week5/travel_2.png?raw=true",
-//     area: "台中",
-//     price: 1280,
-//     num: 5,
-//     rank: 9.2,
-//     describe:
-//       "台中全新親子景點寶熊漁樂碼頭，為知名釣具公司「OKUMA」所創立的觀光工廠。一樓藍白希臘漁村風商店街免費參觀。二樓釣魚故事館則設立全台唯一虛擬釣場，透過導覽讓你知道如何釣魚、魚餌怎麼區分，寓教於樂的台中景點！",
-//   },
-// ];
+AOS.init();
+
 let tickets;
 const btnAddTicket = document.querySelector("#btnAddTicket");
+const inputValues = document.querySelectorAll(".input");
 
 // 新增套票 (改練習 querySelectorAll() 獲取所有 input)
 btnAddTicket.addEventListener("click", addTicket);
 function addTicket() {
-  const inputValues = document.querySelectorAll(".input");
-  let isTicketData;
   const ticketData = {};
+  let isTicketData = false;
+
   inputValues.forEach((item) => {
-    isTicketData = item.value !== "" ? true : false;
+    isTicketData =
+      ticketName.value !== "" &&
+      ticketImgUrl.value !== "" &&
+      scenicArea.value !== "" &&
+      ticketPrice.value !== "" &&
+      ticketNum.value !== "" &&
+      ticketRank.value !== "" &&
+      ticketDescribe.value !== "";
+
     ticketData.id = tickets.length + 1;
     if (item.getAttribute("id") === "ticketName") {
       ticketData.name = item.value;
@@ -72,50 +44,12 @@ function addTicket() {
     tickets.push(ticketData);
     form.reset(); // 新增完，清空表單
     render(tickets);
+    initRequired();
   } else {
-    alert("請輸入套票資訊");
+    // alert("請輸入套票完整資訊");
+    checkRequired(inputValues);
   }
 }
-
-// btnAddTicket.addEventListener("click", () => {
-//   const ticketName = document.querySelector("#ticketName");
-//   const ticketImgUrl = document.querySelector("#ticketImgUrl");
-//   const scenicArea = document.querySelector("#scenicArea");
-//   const ticketPrice = document.querySelector("#ticketPrice");
-//   const ticketNum = document.querySelector("#ticketNum");
-//   const ticketRank = document.querySelector("#ticketRank");
-//   const ticketDescribe = document.querySelector("#ticketDescribe");
-
-//   const ticketData = {
-//     id: tickets.length + 1,
-//     name: ticketName.value,
-//     imgUrl: ticketImgUrl.value,
-//     area: scenicArea.value,
-//     price: Number(ticketPrice.value),
-//     group: Number(ticketNum.value),
-//     rate: Number(ticketRank.value),
-//     description: ticketDescribe.value,
-//   };
-
-//   const isTicketData =
-//     ticketName.value !== "" &&
-//     ticketImgUrl.value !== "" &&
-//     scenicArea.value !== "" &&
-//     ticketPrice.value !== "" &&
-//     ticketNum.value !== "" &&
-//     ticketRank.value !== "" &&
-//     ticketDescribe.value !== "";
-
-//   // 有輸入套票訊息才能新增
-//   if (isTicketData) {
-//     const form = document.querySelector("form");
-//     tickets.push(ticketData);
-//     form.reset(); // 新增完，清空表單
-//     render(tickets);
-//   } else {
-//     alert("請輸入套票資訊");
-//   }
-// });
 
 // 渲染畫面(傳入的參數為 原始套票陣列 或 篩選地區後的套票陣列)
 function render(ticketsArr) {
@@ -168,6 +102,7 @@ function render(ticketsArr) {
   });
   ticketList.innerHTML = str;
   searchResult.textContent = `本次搜尋共 ${ticketsArr.length} 筆資料`;
+  getChart(ticketsArr);
 }
 
 // 地區篩選
@@ -196,9 +131,69 @@ function init() {
     .then(function (res) {
       tickets = res.data.data;
       render(tickets);
+      getChart(tickets);
     })
     .catch(function (err) {
       console.log(err);
     });
 }
 init();
+
+let chartData = [];
+
+// C3 圓餅圖
+function getChart(tickets) {
+  const areaData = {};
+  tickets.forEach((item) => {
+    areaData[item.area] ? areaData[item.area]++ : (areaData[item.area] = 1);
+  });
+  chartData = Object.entries(areaData);
+  // [["高雄", 2], ["台北",1], ["台中", 1]]
+
+  const chart = c3.generate({
+    bindto: "#chart",
+    data: {
+      columns: chartData,
+      type: "donut",
+      colors: {
+        高雄: "#E68619",
+        台北: "#26BFC7",
+        台中: "#5151D3",
+      },
+    },
+    donut: {
+      title: "地區比例",
+      width: 20, // donut 寬度
+      label: {
+        // 隱藏圖表直接顯示的 % 數
+        show: false,
+      },
+    },
+    size: {
+      // 整體寬高
+      height: 200,
+      width: 200,
+    },
+  });
+}
+
+// 驗證必填樣式
+function checkRequired(inputValues) {
+  inputValues.forEach((item) => {
+    const span =
+      item.parentNode.previousSibling.previousSibling.lastElementChild;
+    if (!item.value) {
+      span.classList.remove("d-none");
+    } else {
+      span.classList.add("d-none");
+    }
+  });
+}
+// 初始化必填樣式
+function initRequired() {
+  inputValues.forEach((item) => {
+    const span =
+      item.parentNode.previousSibling.previousSibling.lastElementChild;
+    span.classList.add("d-none");
+  });
+}
